ddebc54bcdb63505969e3d361996a1ce10b9cede
joakim bengtson <joakim.bson@gmail.com>
2018-03-14 18:57:18 +0100

    -

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ var Server = function(args) {
 		
 
 		app.listen(app.get('port'), function() {
-			console.log("SPY Lamp is running on port " + app.get('port'));
+			console.log("JBN SPY Lamp is running on port " + app.get('port'));
 		});
 
 	};
